22d76e39728066c74932c870c112aa1f0fb7eb0d
getch04 <getbet04@gmail.com>
2022-03-19 02:01:39 +0300

    feat: add folder scructure

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure() : super();
+}
+
+class ServerFailure extends Failure {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+}
+
+class CacheFailure extends Failure {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+}
