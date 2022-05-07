6ebd02a8995d04fc9a344fe038a4f25248a9e2fe
get_ <getme04@gmail.com>
2022-05-07 13:09:18 +0300

    chore: clean the code based on  given comment

--- lib/core/presentation/widgets/custom_circular_button.dart
+++ lib/core/presentation/widgets/custom_circular_button.dart
@@ -3,9 +3,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:uome2/core/presentation/themes/app_colors.dart';
 
 class CustomCircularButton extends StatelessWidget {
-  const CustomCircularButton(
-      {Key? key, required this.onPressed, required this.flatIcon})
-      : super(key: key);
+  const CustomCircularButton({
+    Key? key,
+    required this.onPressed,
+    required this.flatIcon,
+  }) : super(key: key);
   final Function onPressed;
   final IconData flatIcon;
 
